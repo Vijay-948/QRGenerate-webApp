92f8c6d649a962cce6d6100d10de3175c43434bd
Vijay <kvijayreddy948@gmail.com>
2022-09-27 23:14:46 +0530

    First commit

---
+++ src/main/java/com/MySpringproject/Springbootqrwebapp/SpringBootQrWebappApplication.java
@@ -0,0 +1,13 @@
+package com.MySpringproject.Springbootqrwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MySpringproject/Springbootqrwebapp/SpringBootQrWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MySpringproject.Springbootqrwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
